65b6cc1c89e2ef9cdaa77586c58ed889dd8f603f
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-10 16:06:37 +0530

    Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,5 +5,28 @@
             <li><router-link :to="{ name: 'About'}">About</router-link></li>
         </ul>
     </nav>
-        
 </template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+
+ul{
+    list-style-type: none;
+    padding: 0;
+}
+
+a{
+    color: lightgreen;
+}
+    
+</style>
